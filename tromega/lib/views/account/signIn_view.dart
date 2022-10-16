599a7b1235c1d5b24e1039416db49dfdc18f61c1
Rebekka Miguez <rebekka.miguez@gmx.de>
2022-10-16 18:38:19 +0200

    google_login_test

--- tromega/lib/views/account/signIn_view.dart
+++ tromega/lib/views/account/signIn_view.dart
@@ -4,23 +4,22 @@ import 'dart:convert' show json;
 import 'package:flutter/material.dart';
 import 'package:google_sign_in/google_sign_in.dart';
 import 'package:http/http.dart' as http;
-import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
-GoogleSignIn _googleSignIn = GoogleSignIn(
+final GoogleSignIn _googleSignIn = GoogleSignIn(
   scopes: <String>[
     'email',
     'https://www.googleapis.com/auth/contacts.readonly',
   ],
 );
 
-class LoginView extends StatefulWidget {
-  const LoginView({super.key});
+class SignInView extends StatefulWidget {
+  const SignInView({Key? key}) : super(key: key);
 
   @override
-  State<LoginView> createState() => _LoginViewState();
+  State<SignInView> createState() => _SignInViewState();
 }
 
-class _LoginViewState extends State<LoginView> {
+class _SignInViewState extends State<SignInView> {
   GoogleSignInAccount? _currentUser;
   String _contactText = '';
 
@@ -121,26 +120,15 @@ class _LoginViewState extends State<LoginView> {
         ],
       );
     } else {
-      return Padding(
-        padding: const EdgeInsets.all(15),
-        child: ElevatedButton.icon(
-          icon: const FaIcon(
-            FontAwesomeIcons.google,
-            //color: Color.fromARGB(1000, 240, 157, 2)
+      return Column(
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        children: <Widget>[
+          const Text('You are not currently signed in.'),
+          ElevatedButton(
+            onPressed: _handleSignIn,
+            child: const Text('SIGN IN'),
           ),
-          style: ElevatedButton.styleFrom(
-            minimumSize: const Size(200, 50),
-            maximumSize: const Size(200, 50),
-            primary: Color.fromARGB(1000, 0, 48, 80),
-          ),
-          onPressed: _handleSignIn,
-          label: const Text(
-            'Sign Up with Google',
-            style: TextStyle(
-              fontWeight: FontWeight.bold,
-            ),
-          ),
-        ),
+        ],
       );
     }
   }
@@ -148,19 +136,12 @@ class _LoginViewState extends State<LoginView> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: ListView(
-        children: [
-          SizedBox(
-            width: 300,
-            height: 300,
-            child: Image.asset(
-              'public/TrOmega_dark.png',
-            ),
-          ),
-          const SizedBox(height: 24),
-          _buildBody()
-        ],
-      ),
-    );
+        appBar: AppBar(
+          title: const Text('Google Sign In'),
+        ),
+        body: ConstrainedBox(
+          constraints: const BoxConstraints.expand(),
+          child: _buildBody(),
+        ));
   }
 }
