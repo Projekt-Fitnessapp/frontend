bd558c3d18c8b3ca23762a49e009691151cd88d3
TobiasFitzke01 <84978037+TobiasFitzke01@users.noreply.github.com>
2023-03-14 22:43:34 +0100

    anpassung Übergabeparameter zum Backend

--- tromega/lib/data/classes/generated_plan_preferences.dart
+++ tromega/lib/data/classes/generated_plan_preferences.dart
@@ -1,14 +1,14 @@
 class GeneratedPlanPreferences {
   late String _id, trainingsStatus, trainingsType;
-  late int numberOfTrainingssession;
+  late int numberOfTrainingssessions;
 
-  GeneratedPlanPreferences(this._id, this.numberOfTrainingssession,
+  GeneratedPlanPreferences(this._id, this.numberOfTrainingssessions,
       this.trainingsStatus, this.trainingsType);
 
   Map toJSON() {
     return {
       'user_id': _id,
-      'numberOfTrainingssession': numberOfTrainingssession,
+      'numberOfTrainingssessions': numberOfTrainingssessions,
       'trainingsStatus': trainingsStatus,
       'trainingsType': trainingsType
     };
